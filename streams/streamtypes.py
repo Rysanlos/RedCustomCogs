1ba618a2b95dfee324b1c234e18d39e333d79f87
Rysanlos <contact@leo-fr.net>
2022-01-01 15:08:12 +0100

    Fixed tabulation
    Original project: https://github.com/Cog-Creators/Red-DiscordBot/tree/V3/develop/redbot/cogs/streams

--- streams/streamtypes.py
+++ streams/streamtypes.py
@@ -450,14 +450,14 @@ class TwitchStream(Stream):
         embed.set_author(name=data["user_name"], icon_url=logo)
         #embed.add_field(name=_("Followers"), value=humanize_number(data["followers"]))
         #embed.add_field(name=_("Total views"), value=humanize_number(data["view_count"]))
-		if data["game_name"]:
-			embed.add_field(name=_("Catégorie"), value=data["game_name"])
+	if data["game_name"]:
+	    embed.add_field(name=_("Catégorie"), value=data["game_name"])
         #embed.set_thumbnail(url=logo)
         if data["thumbnail_url"]:
             embed.set_image(url=rnd(data["thumbnail_url"].format(width=640, height=360)))
         #if data["game_name"]:
         #    embed.set_footer(text=_("Playing: ") + data["game_name"])
-		embed.set_footer(text=_("twitch.tv/") + data['login'])
+	embed.set_footer(text=_("twitch.tv/") + data['login'])
         return embed
 
     def __repr__(self):
